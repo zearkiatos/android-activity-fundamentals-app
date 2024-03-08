4b34987b2babc1b836bd9f465b9f0e5a0d8814fc
zearkiatos <caprilespe@outlook.com>
2024-03-07 21:29:02 -0300

    Initializate project

---
+++ app/src/main/java/com/android/aicolor/ui/DashboardActivity.kt
@@ -0,0 +1,12 @@
+package com.android.aicolor.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.aicolor.R
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
